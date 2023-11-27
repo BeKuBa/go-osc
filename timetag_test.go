39b0d637b4eee39097e293852dc78bbefff2c02f
stanipetrosyan <stani.petrosyan@gmail.com>
2023-11-27 13:58:00 +0100

    test: added test to timetag

--- timetag_test.go
+++ timetag_test.go
@@ -10,12 +10,14 @@ import (
 func TestTimetag(t *testing.T) {
 	t.Run("should create an immediate timetag", func(t *testing.T) {
 		tt := NewImmediateTimetag()
+
 		assert.Equal(t, tt.Time(), time.Date(1900, time.January, 1, 1, 0, 0, 1, time.Local))
 	})
 
 	t.Run("should create a TimeTag", func(t *testing.T) {
 		ti := time.Now()
 		tt := NewTimetagFromTime(ti)
+
 		assert.True(t, tt.Time().Equal(ti))
 	})
 
@@ -26,5 +28,14 @@ func TestTimetag(t *testing.T) {
 		actual := tt.ExpiresIn().Round(time.Millisecond)
 
 		assert.True(t, actual == 60*time.Second)
+	})
+
+	t.Run("should marshall binary an immediate tag", func(t *testing.T) {
+		tt := NewImmediateTimetag()
+
+		actual, err := tt.MarshalBinary()
+		assert.Nil(t, err)
+
+		assert.Equal(t, []byte{0, 0, 0, 0, 0, 0, 0, 1}, actual)
 	})
 }
