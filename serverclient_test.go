4d333782881ed95344f0c64207ad1c7932852f41
BeKuBa <bb@bekuba.de>
2024-02-02 13:15:54 +0100

    improve use of int types in SendMsg

--- serverclient_test.go
+++ serverclient_test.go
@@ -1,12 +1,13 @@
 package osc_test
 
 import (
-	"github.com/crgimenes/go-osc"
-	"github.com/stretchr/testify/assert"
 	"net"
 	"sync"
 	"testing"
 	"time"
+
+	"github.com/crgimenes/go-osc"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -24,8 +25,35 @@ func TestServerAndClient(t *testing.T) {
 		wait := sync.WaitGroup{}
 		wait.Add(1)
 
-		var d float64 = 0.0
-		var i int32 = 0
+		var pingF64 = 0.0
+
+		var boolTrue = false
+		var boolFalse = true
+		var i32 int32 = 0
+		var i64 int64 = 0
+		var f32 float32 = 0.0
+		var f64 float64 = 0.0
+		var strTest string = ""
+		var strEmpty string = "e"
+		var i int = 0
+		var null any = 10
+		var array []byte = []byte{10, 11, 12}
+		var timetag osc.Timetag
+
+		const (
+			cBoolTrue          = true
+			cBoolFalse         = false
+			cI32       int32   = 2
+			cI64       int64   = 3
+			cF32       float32 = 4.0
+			cF64       float64 = 5.0
+			cStrTest   string  = "6test"
+			cStrEmpty  string  = ""
+			cI         int     = 8
+			// nil
+		)
+		var cArray []byte = []byte{10, 48}
+		const cTimetag osc.Timetag = 16818286200017484014
 
 		addr1, err := net.ResolveUDPAddr("udp", "127.0.0.1:8000")
 		if err != nil {
@@ -40,19 +68,12 @@ func TestServerAndClient(t *testing.T) {
 		d1 := osc.NewStandardDispatcher()
 		app1 := osc.NewServerAndClient(d1)
 		err = app1.NewConn(addr2, addr1)
-		if err != nil {
-			t.Error(err)
-		}
-		defer func() {
-			err := app1.Close()
-			if err != nil {
-				t.Error(err)
-			}
-		}()
 
 		err = d1.AddMsgHandler(ping, func(msg *osc.Message) {
-			d = msg.Arguments[0].(float64)
-			err = app1.SendMsg(pong, 2)
+			pingF64 = msg.Arguments[0].(float64)
+
+			err = app1.SendMsg(pong, cBoolTrue, cBoolFalse, cI32, cI64, cF32, cF64, cStrTest, cStrEmpty, cI, nil, cArray, cTimetag)
+
 			if err != nil {
 				t.Error(err)
 			}
@@ -70,7 +91,20 @@ func TestServerAndClient(t *testing.T) {
 
 		d2 := osc.NewStandardDispatcher()
 		err = d2.AddMsgHandler(pong, func(msg *osc.Message) {
-			i = msg.Arguments[0].(int32)
+
+			boolTrue = msg.Arguments[0].(bool)
+			boolFalse = msg.Arguments[1].(bool)
+			i32 = msg.Arguments[2].(int32)
+			i64 = msg.Arguments[3].(int64)
+			f32 = msg.Arguments[4].(float32)
+			f64 = msg.Arguments[5].(float64)
+			strTest = msg.Arguments[6].(string)
+			strEmpty = msg.Arguments[7].(string)
+			i = int(msg.Arguments[8].(int32))
+			null = msg.Arguments[9]
+			array = msg.Arguments[10].([]byte)
+			timetag = msg.Arguments[11].(osc.Timetag)
+
 			wait.Done()
 		})
 		if err != nil {
@@ -103,8 +137,20 @@ func TestServerAndClient(t *testing.T) {
 		wait.Wait()
 
 		// check if send and receive are the same
-		assert.Equal(t, 1.0, d)
-		assert.Equal(t, int32(2), i)
+		assert.Equal(t, 1.0, pingF64)
+
+		assert.Equal(t, cBoolTrue, boolTrue)
+		assert.Equal(t, cBoolFalse, boolFalse)
+		assert.Equal(t, cI32, i32)
+		assert.Equal(t, cI64, i64)
+		assert.Equal(t, cF32, f32)
+		assert.Equal(t, cF64, f64)
+		assert.Equal(t, cStrTest, strTest)
+		assert.Equal(t, cStrEmpty, strEmpty)
+		assert.Equal(t, cI, i)
+		assert.Nil(t, null)
+		assert.Equal(t, cArray, array)
+		assert.Equal(t, cTimetag, timetag)
 
 		done <- true
 	}()
